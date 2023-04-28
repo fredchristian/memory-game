073caf2b25e4c0d5fb804fd39000ca230e43fbcf
Fred Christian <fred.christian@gmail.com>
2023-04-28 15:13:47 +0200

    Update index.blade.php
    - modification du responsive pour le "menu"

--- resources/views/components/menu/index.blade.php
+++ resources/views/components/menu/index.blade.php
@@ -1,11 +1,11 @@
-<div class="space-y-2 sm:space-y-0">
-    <div class="sm:hidden flex justify-center">
+<div class="space-y-2 md:space-y-0">
+    <div class="md:hidden flex justify-center">
         <x-menu.characters class="flex" />
     </div>
 
     <div class="flex-initial flex items-center justify-between space-x-8 py-4">
         <x-menu.score />
-        <x-menu.characters class="hidden sm:flex" />
+        <x-menu.characters class="hidden md:flex" />
         <x-menu.cancel />
     </div>
 </div>
